e726c071aed40373fb91583fefe9d7629369175e
SizanMahmud69 <sizanmahmud369@gmail.com>
2026-05-04 14:09:29 +0600

    Add files via upload

---
+++ src/constants.ts
@@ -0,0 +1,9 @@
+export const DEFAULT_CONFIG = {
+  pointsPerBDT: 1000,
+  minWithdrawalPoints: 5000,
+  spinPointsRange: { min: 10, max: 100 },
+  scratchPointsRange: { min: 5, max: 50 },
+};
+
+export const SPIN_COOLDOWN = 60 * 60 * 1000; // 1 hour
+export const SCRATCH_COOLDOWN = 30 * 60 * 1000; // 30 minutes
